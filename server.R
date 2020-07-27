24abf753989d57a40b86a8680869efd230eecf56
thistleknot <laferrierejc@gmail.com>
2020-07-27 00:14:27 +0000

    reactive and observe vars

--- server.R
+++ server.R
@@ -1,9 +1,34 @@
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
 print(str(bcl))
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  filtered <- reactive({
+    bcl %>%
+      filter(Price >= input$priceInput[1],
+             Price <= input$priceInput[2],
+             Type == input$typeInput,
+             Country == input$countryInput
+      )
+  })
+  
+  output$results <- renderDataTable({
+    filtered()
+  })
+  
+  output$coolplot <- renderPlot({
+    ggplot(filtered(), aes(Alcohol_Content)) +
+      geom_histogram()
+    
+  })
+  #testing error
+  observe({print(input$priceInput)})
+  priceDiff <- reactive({diff(input$priceInput)})
+  observe({print(priceDiff())})
+  }
 
 #shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -1,13 +1,22 @@
 ui <- fluidPage(
   titlePanel("BC Liquor Store prices",windowTitle = "title"),
   sidebarLayout(
+    #inputs
     sidebarPanel( sliderInput("priceInput", "Price", min = 0, max = 100,
                               value = c(25, 40), pre = "$"),
                   radioButtons("typeInput", "Product type",
                                #choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                                choices = c(unique(bcl$Type)),
-                               selected = "WINE")),
-    mainPanel("the results will go here")
+                               selected = "WINE"),
+                  selectInput("countryInput", "Country",
+                              choices = c(unique(bcl$Country)))
+                  ),
+    #outputs
+    mainPanel(
+      plotOutput("coolplot"),
+      br(), br(),
+      dataTableOutput("results")
+      )
   )
   
   
